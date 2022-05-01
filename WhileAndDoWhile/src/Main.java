97f8a05c566e7b44af0c5b9d0d8f407eb485fe1e
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-01 21:45:23 +0900

    The while and do while statements

---
+++ WhileAndDoWhile/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        int count = 1;
+//        while(count != 6){
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+
+//        count = 6;
+//        do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//
+//            if(count > 100){
+//                break;
+//            }
+//        } while (count != 6);
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumbersFound = 0;
+
+        while (number <= finishNumber){
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+
+            evenNumbersFound++;
+            System.out.println("Even number " + number);
+
+            if(evenNumbersFound >= 5){
+                break;
+            }
+        }
+
+        System.out.println("Total even numbers found = " + evenNumbersFound);
+    }
+
+    public static boolean isEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
