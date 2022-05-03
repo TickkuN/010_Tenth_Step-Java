cbe92641c0ed3624d011d775053dfbaaf83cc85c
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-03 09:07:39 +0900

    Person

---
+++ Person/src/Person.java
@@ -0,0 +1,50 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setFirstName(String name){
+        this.firstName = name;
+    }
+
+    public void setLastName(String name){
+        this.lastName = name;
+    }
+
+    public void setAge(int age){
+        if((age < 0) || (100 < age)){
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public boolean isTeen(){
+        return (12 < this.age && this.age < 20);
+    }
+
+    public String getFullName(){
+        if(this.firstName.isEmpty() && this.lastName.isEmpty()){
+            return "";
+        } else if (this.firstName.isEmpty()) {
+            return this.lastName;
+        } else if (this.lastName.isEmpty()){
+            return this.firstName;
+        } else {
+            return this.firstName + " " + this.lastName;
+        }
+    }
+}
