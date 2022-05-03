52286303abc6a4192f63dfaa7545054420862ed2
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-04 00:00:29 +0900

    Composition

---
+++ Composition/src/Lamp.java
@@ -0,0 +1,27 @@
+public class Lamp {
+    private String style;
+    private boolean battery;
+    private int globRationg;
+
+    public Lamp(String style, boolean battery, int globRationg) {
+        this.style = style;
+        this.battery = battery;
+        this.globRationg = globRationg;
+    }
+
+    public void turnOn() {
+        System.out.println("Lamp -> Turning on");
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRationg() {
+        return globRationg;
+    }
+}
