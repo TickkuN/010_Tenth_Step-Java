cce7246b7bfcdf1848e67aebdd950556dba8702e
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-03 13:38:54 +0900

    Constructors Part1

---
+++ Constructors/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Account bobsAccount = new Account("12345", 0.00, "Bob Brown", "myemail@bob.com", "(087)123-4567");
+        Account bobsAccount = new Account();
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+
+        bobsAccount.setNumber("12345");
+        bobsAccount.setBalance(0.00);
+        bobsAccount.setCustomerName("Bob Brown");
+        bobsAccount.setCustomerEmailAddress("myemail@bob.com");
+        bobsAccount.setCustomerPhoneNumber("(087) 123-4567");
+
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(50.0);
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(51.0);
+        bobsAccount.withdrawal(100.0);
+    }
+}
