413b1d1fbd7d8e41958b0139b2fbde97ae12eb27
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-04 17:22:36 +0900

    Polymorphism coding

---
+++ PolymorphismCar/src/Holden.java
@@ -0,0 +1,18 @@
+public class Holden extends Car {
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Holden -> engine starts.";
+    }
+    @Override
+    public String accelerate() {
+        return "Holden -> accelerating.";
+    }
+    @Override
+    public String brake() {
+        return "Holden -> braking.";
+    }
+}
---
+++ PolymorphismCar/src/Mitsubishi.java
@@ -0,0 +1,18 @@
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+
+    }
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> engine starts.";
+    }
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerating.";
+    }
+    @Override
+    public String brake() {
+        return "Mitsubishi -> braking.";
+    }
+}
