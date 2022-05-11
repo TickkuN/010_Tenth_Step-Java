dd1c0e6fcae41fe3065ac86692ef6af9731969d6
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-12 02:36:41 +0900

    Interfaces Challenge Part 2

--- InterfacesChallenge/src/Main.java
+++ InterfacesChallenge/src/Main.java
@@ -12,8 +12,12 @@ public class Main {
         System.out.println(tim);
         tim.setWeapon("Stormbringer");
         saveObject(tim);
-        loadObject(tim);
+//        loadObject(tim);
         System.out.println(tim);
+
+        ISaveable werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println("Strength = " + ((Monster)werewolf).getStrength());
+        saveObject(werewolf);
     }
 
     public static ArrayList<String> readValues() {
