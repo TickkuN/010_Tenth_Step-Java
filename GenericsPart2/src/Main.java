bb32335e0ea336c9729971d58fbdf5e69283b9d3
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-15 09:31:37 +0900

    Our Generics Class Part 3

--- GenericsPart2/src/Main.java
+++ GenericsPart2/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
         FootballPlayer joe = new FootballPlayer("joe");
@@ -28,6 +31,20 @@ public class Main {
         hawthorn.matchResult(adelaideCrows, 3, 8);
 
         adelaideCrows.matchResult(fremantle, 2, 1);
-        adelaideCrows.matchResult(baseballPlayerTeam, 1, 1);
+//        adelaideCrows.matchResult(baseballPlayerTeam, 1, 1);
+
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
+
+        ArrayList<Team> teams;
+        Collections.sort(teams);
     }
 }
