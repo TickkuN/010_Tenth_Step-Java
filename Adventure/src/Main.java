9a83b3de2dbbf4e16e653df1e51caefb4a3b6d61
TickkuN <Tickkun.addEventListener@gmail.com>
2022-05-21 18:56:41 +0900

    Adding Exits to the Adventure game

--- Adventure/src/Main.java
+++ Adventure/src/Main.java
@@ -15,17 +15,63 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
-        int loc = 1;
-        while (true){
-            System.out.println(locations.get(loc).getDescription());
-            if (loc == 0){
-                break;
-            }
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+//        locations.get(1).addExit("Q", 0);
 
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)){
-                System.out.println("You cannot go in that direction");
-            }
+        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
+
+//        int loc = 1;
+//        while (true){
+//            System.out.println(locations.get(loc).getDescription());
+//            if (loc == 0){
+//                break;
+//            }
+//
+//            Map<String, Integer> exits = locations.get(loc).getExits();
+//            System.out.print("Available exits are ");
+//            for(String exit: exits.keySet()){
+//                System.out.print(exit + ", ");
+//            }
+//            System.out.println();
+//
+//            String direction = scanner.nextLine().toUpperCase();
+//
+//            if(exits.containsKey(direction)){
+//                loc = exits.get(direction);
+//            } else {
+//                System.out.println("You can not go in that direction");
+//            }
+//        }
+
+        String[] roads = "You are standing at the end of a road before a small brick building".split(" ");
+        for(String road: roads){
+            System.out.println(road);
         }
+
+        System.out.println("==============================================");
+
+        String[] buildings = "You are inside a building, a well house for a small spring".split(", ");
+        for(String building: buildings){
+            System.out.println(building);
+        }
+
+
+
     }
 }
